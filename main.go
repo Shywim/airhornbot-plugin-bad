aa9e671c74f74a130e4bc2a50a00a67ec60aaa58
Matthieu Harlé <harle.matthieu@gmail.com>
2017-11-02 22:13:39 +0100

    ✨ Random command

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/jonas747/dca"
 )
@@ -17,6 +22,11 @@ const (
 // Name of your plugin, must be unique from all other plugins
 var Name = "com.matthieuharle.bad"
 
+func randomRange(min, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return rand.Intn(max-min) + min
+}
+
 // Handle is called to check if your plugin can handle the sound
 // if you return true here, GetSound is expected to return valid sound data
 func Handle(name string) bool {
@@ -24,7 +34,14 @@ func Handle(name string) bool {
 		return false
 	}
 
-	sound := strings.TrimLeft(name, soundPrefix)
+	sound := strings.TrimPrefix(name, soundPrefix)
+	if sound == "salut" {
+		return true
+	}
+	if sound == "_random" {
+		return true
+	}
+
 	resp, err := http.Head(url + sound + urlSuffix)
 	if err != nil {
 		return false
@@ -37,7 +54,29 @@ func Handle(name string) bool {
 
 // GetSound is called when one of your sound will be played
 func GetSound(name string) (buffer [][]byte) {
-	sound := strings.TrimLeft(name, soundPrefix)
+	sound := strings.TrimPrefix(name, soundPrefix)
+	if sound == "salut" {
+		sound = sound + strconv.Itoa(randomRange(1, 37))
+	} else if sound == "_random" {
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil
+		}
+		index := string(body)
+		resp.Body.Close()
+
+		re := regexp.MustCompile(">(.*?).ogg")
+		allSounds := re.FindAllStringSubmatch(index, -1)
+		if allSounds == nil {
+			return nil
+		}
+		sound = allSounds[randomRange(0, len(allSounds))][1]
+	}
+
 	resp, err := http.Get(url + sound + urlSuffix)
 	if err != nil {
 		return nil
